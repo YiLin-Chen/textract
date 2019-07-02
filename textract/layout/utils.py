6ded717fff8bf4a773f87445c16afad9a22d96b8
Yi-Lin Chen <lin781219@gmail.com>
2019-07-02 04:04:06 +0000

    change indent to 4 space

--- textract/layout/utils.py
+++ textract/layout/utils.py
@@ -1,49 +1,49 @@
 from enum import Enum
 
 class LayoutType(Enum):
-	Page = 0
-	Paragraph = 1
-	Line = 2
-	Image = 3
+    Page = 0
+    Paragraph = 1
+    Line = 2
+    Image = 3
 
 class LayoutUtils():
-	def __init__(self, id_, src, type_):
-		self._id   = id_
-		self._src  = src
-		self._type = type_
+    def __init__(self, id_, src, type_):
+        self._id   = id_
+        self._src  = src
+        self._type = type_
 
-	def get_id(self):
-		return self._id
+    def get_id(self):
+        return self._id
 
-	def get_src(self):
-		return self._src
+    def get_src(self):
+        return self._src
 
-	def get_type(self):
-		return self._type
+    def get_type(self):
+        return self._type
 
 class Page(LayoutUtils):
-	def __init__(self, id_, src):
-		super().__init__(id_, src, LayoutType.Page)
+    def __init__(self, id_, src):
+        super().__init__(id_, src, LayoutType.Page)
 
 class Paragraph(LayoutUtils):
-	def __init__(self, id_, src):
-		super().__init__(id_, src, LayoutType.Paragraph)
+    def __init__(self, id_, src):
+        super().__init__(id_, src, LayoutType.Paragraph)
 
 class Line(LayoutUtils):
-	def __init__(self, id_, src):
-		super().__init__(id_, src, LayoutType.Line)
+    def __init__(self, id_, src):
+        super().__init__(id_, src, LayoutType.Line)
 
 class Image(LayoutUtils):
-	def __init__(self, id_, src):
-		super().__init__(id_, src, LayoutType.Image)
+    def __init__(self, id_, src):
+        super().__init__(id_, src, LayoutType.Image)
 
 class BBox(object):
-	def __init__(self, id_, x, y, w, h):
-		self._id = id_
-		self._coords = (x, y, x+w, y+h)
+    def __init__(self, id_, x, y, w, h):
+        self._id = id_
+        self._coords = (x, y, x+w, y+h)
 
-	def get_id(self):
-		return self._id
+    def get_id(self):
+        return self._id
 
-	def get_coords(self):
-		return self._coords
+    def get_coords(self):
+        return self._coords
