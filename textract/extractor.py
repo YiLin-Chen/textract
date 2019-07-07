921501936281b1fdc01942dca89ed01ae15d6541
Yi-Lin Chen <lin781219@gmail.com>
2019-07-07 02:01:36 +0000

    add comment into extractor

--- textract/extractor.py
+++ textract/extractor.py
@@ -1,5 +1,5 @@
 '''
-higher level class for api
+ectractor.py is a module that provides high level api for extracting text
 '''
 
 import cv2, os
@@ -10,20 +10,31 @@ from textract.model.ocr import TextRecognizer, RecognizerType
 
 
 class TextExtractor(object):
-    def __init__(self, ocr_model = RecognizerType.GOOGLE):
+    '''
+    a class used for extracting text from an image
+    '''
+    def __init__(self, ocr_model = RecognizerType.GOOGLE): 
         self._ocr_model = ocr_model
         self._analyzer = LayoutAnalyzer()
         self._recognizer = TextRecognizer(ocr_model)
 
 
     def extract(self, image):
+        '''
+        Desc: extract text string from an image
+
+        Args:
+            - image (numpy.ndarray): target image source
+
+        Returns:
+            - res_str (List[str]): a list of paragraph text that is extracted from an image
+        '''
+       
         res_str = []
         
-        # image = self._analyzer.deskew(image)
         # analyze layout
         page = Page(0, image)
         paragraphs = self._analyzer.extract(page)
-
 
         for para in paragraphs:
             if para.get_type() == LayoutType.Paragraph:
@@ -43,7 +54,6 @@ class TextExtractor(object):
 
                     ocr_text = ocr_text[:-1]
 
-    
                 res_str.append(ocr_text)
                 
         return res_str
